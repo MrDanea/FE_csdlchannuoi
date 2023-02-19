43082b71aacba97c4c7ca1853f88a8fff0b371cf
MrDanea <86879739+MrDanea@users.noreply.github.com>
2023-02-19 10:23:17 +0700

    Design1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Design.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var dlg = new LoginWindowView();
+            if (dlg.ShowDialog() == false)
+            {
+                App.Current.Shutdown();
+            }
+
+            InitializeComponent();
+        }
+
+        
+    }
+}
